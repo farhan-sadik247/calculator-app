d6136141492127ea259121d19b4b00138941eaf4
MD. FARHAN SADIK <md.farhan.sadik@g.bracu.ac.bd>
2025-05-20 12:51:54 +0000

    number is only 0,4,5,6,7,8,9. must include 1,2,3 too.

--- src/components/calculator/Keypad.tsx
+++ src/components/calculator/Keypad.tsx
@@ -38,11 +38,15 @@ const buttonsConfig = [
   { label: '6', value: '6', type: 'number', sr: 'Number 6' },
   { label: '+', value: '+', type: 'operator', icon: <Plus size={20}/>, sr: 'Addition operator' },
   { label: '-', value: '-', type: 'operator', icon: <Minus size={20}/>, sr: 'Subtraction operator' },
-  // Row 6
-  { label: '0', value: '0', type: 'number', className: "col-span-2", sr: 'Number 0' },
-  { label: '.', value: '.', type: 'number', sr: 'Decimal point' },
+  // Row 6 (New row with 1, 2, 3)
+  { label: '1', value: '1', type: 'number', sr: 'Number 1' },
+  { label: '2', value: '2', type: 'number', sr: 'Number 2' },
+  { label: '3', value: '3', type: 'number', sr: 'Number 3' },
   { label: '+/-', value: '+/-', type: 'special', sr: 'Toggle sign' },
   { label: '=', value: '=', type: 'equals', icon: <Equal size={20}/>, sr: 'Equals button' },
+  // Row 7 (Modified last row)
+  { label: '0', value: '0', type: 'number', className: "col-span-3", sr: 'Number 0' },
+  { label: '.', value: '.', type: 'number', className: "col-span-2", sr: 'Decimal point' },
 ];
 
 export function Keypad({ onButtonClick, isRadians }: KeypadProps) {
